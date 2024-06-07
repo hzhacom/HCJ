a2c0791d50872aa901835d7568a35e019d92ef3c
hzhacom <78690860@qq.com>
2024-06-07 12:19:46 +0800

    Add files via upload

---
+++ ! J.js
@@ -0,0 +1,9 @@
+﻿const button = document.querySelector("button");
+
+function greet() {
+    const name = prompt("你叫什么名字？ ");
+    const greeting = document.querySelector("#g");
+    greeting.textContent = `你好呀，${name}! 很高兴见到你！`;
+}
+
+button.addEventListener("click",greet);
